4b6768ffe49daf0116249bcf5d9abb6af50540c5
Gitika Jain <89632284+gitikaajain@users.noreply.github.com>
2022-11-13 10:43:21 +0530

    Update palindrome_linked_list.cpp

--- 234. Palindrome linked list/palindrome_linked_list.cpp
+++ 234. Palindrome linked list/palindrome_linked_list.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
-    ListNode* temp;
     bool isPalindrome(ListNode* head) {
-        temp = head;
-        return check(head);
+        if(head==NULL || head-> next==NULL) 
+            return true;
+        //slow and fast pointers to find the middle of linked list
+        ListNode* slow=head;
+        ListNode* fast= head;
+        
+        while(fast->next!= NULL && fast ->next->next!=NULL) {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        //reverse the right half of the linked list
+        slow->next=reverseList(slow->next);
+        // pointing slow to the first position of the right half
+        slow=slow->next;
+        //start moving slow and head it will compare right and left component of linked list
+        while(slow!=NULL) {
+            if(head->val!=slow->val)
+                return false;
+            head=head->next;
+            slow=slow->next;
+        }
+        return true;
+        
     }
-    
-    bool check(ListNode* p) {
-        if (NULL == p) return true;
-        bool isPal = check(p->next) & (temp->val == p->val);
-        temp = temp->next;
-        return isPal;
+    //function to reverse the linked list
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre=NULL;
+        ListNode* next=NULL;
+        while(head!=NULL) {
+            next=head->next;
+            head->next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
     }
 };
